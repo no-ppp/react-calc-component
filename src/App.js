c3f73e052994038887592f59f81f3963f75ae296
ppp <pppga99@gmail.com>
2024-11-07 17:13:04 +0100

    Adding tailwind to project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NumButton }  from "./NumButton";
 
 function App() {
   return (
-      <div>
+      <div className='min-h-screen flex items-center justify-center bg-gray-100'>
           <NumButton> 1 </NumButton>
       </div>
   )
