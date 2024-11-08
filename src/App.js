ef55906adcb7a5ec2ca13c037f6f0ad7ad6f1754
ppp <pppga99@gmail.com>
2024-11-08 12:24:34 +0100

    Cleaned code to start working on reducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import {NumButton} from "./NumButton";
 import {OperatorButton} from "./OperatorButton";
 
