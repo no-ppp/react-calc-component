c02bf2da23541fd4cf0d51614ad51ea0339391c0
ppp <pppga99@gmail.com>
2024-11-08 12:24:05 +0100

    changing useState to useReducer to simplify

--- src/App.js
+++ src/App.js
@@ -1,77 +1,35 @@
 import React, { useState } from 'react';
+import {NumButton} from "./NumButton";
+import {OperatorButton} from "./OperatorButton";
 
 function App() {
-    const [shower, setShower] = useState('');
-    const [current, setCurrent] = useState('');
-    const [previous, setPrevious] = useState('');
-    const [operator, setOperator] = useState('');
-    const [result, setResult] = useState('');
-    function clickHandler(input){
-        setCurrent(prevCurrent => {
-            const newCurrent = prevCurrent + input;
-            setShower(result + operator + newCurrent);
-            return newCurrent;
-        });
-
-    }
-    function clearAll(){
-        setCurrent('');
-        setPrevious('');
-        setOperator('');
-        setResult('');
-        setShower('')
-    }
-    function operatorHandler(input){
-        if (operator === ''){
-            setOperator(input)
-            setPrevious(current)
-            setCurrent('')
-            setResult(result + current)
-            setShower( result + operator + current)
-            return;
-        } else {
-
-        }
-
-    }
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-600">
             <div className="bg-gray-700 p-8 rounded-lg shadow-lg">
                 <div className="bg-green-500 pb-2 pt-2 pl-2 h-16">
                     <div className="text-white text-4xl">
-                        { shower }
+                        {'1'}
                     </div>
                 </div>
                 <div className="pt-5 grid grid-cols-4 gap-4">
 
-                    <button onClick={() => clickHandler('1')}
-                            className="bg-gray-500 rounded-full text-white w-16 h-16">1</button>
-                    <button
-                        onClick={() => clickHandler('2')}
-                        className="bg-gray-500
-                        rounded-full
-                        text-white
-                        w-16 h-16"
-                        > 2
-                    </button>
-                    <button onClick={() => clickHandler('3')} className="bg-gray-500 rounded-full text-white w-16 h-16">3</button>
-                    <button onClick={() => {clickHandler('+'); operatorHandler('+')}} className="bg-gray-500 rounded-full text-white w-16 h-16">+</button>
+                    <NumButton>1</NumButton>
+                    <NumButton>2</NumButton>
+                    <NumButton>3</NumButton>
+                    <OperatorButton>+</OperatorButton>
+                    <NumButton>4</NumButton>
+                    <NumButton>5</NumButton>
+                    <NumButton>6</NumButton>
+                    <OperatorButton>+</OperatorButton>
+                    <NumButton>7</NumButton>
+                    <NumButton>8</NumButton>
+                    <NumButton>9</NumButton>
+                    <OperatorButton>/</OperatorButton>
+                    <NumButton>0</NumButton>
+                    <NumButton>,</NumButton>
+                    <OperatorButton>=</OperatorButton>
 
-                    <button onClick={() => clickHandler('4')} className="bg-gray-500 rounded-full text-white w-16 h-16">4</button>
-                    <button onClick={() => clickHandler('5')} className="bg-gray-500 rounded-full text-white w-16 h-16">5</button>
-                    <button onClick={() => clickHandler('6')} className="bg-gray-500 rounded-full text-white w-16 h-16">6</button>
-                    <button  className="bg-gray-500 rounded-full text-white w-16 h-16">-</button>
-
-                    <button onClick={() => clickHandler('7')} className="bg-gray-500 rounded-full text-white w-16 h-16">7</button>
-                    <button onClick={() => clickHandler('8')} className="bg-gray-500 rounded-full text-white w-16 h-16">8</button>
-                    <button onClick={() => clickHandler('9')} className="bg-gray-500 rounded-full text-white w-16 h-16">9</button>
-                    <button className="bg-gray-500 rounded-full text-white w-16 h-16">*</button>
-
-                    <button onClick={() => clickHandler('0')} className="bg-gray-500 rounded-full text-white w-16 h-16">0</button>
-                    <button onClick={() => clearAll()} className="bg-gray-500 rounded-full text-white w-16 h-16">C</button>
-                    <button  className="bg-gray-500 rounded-full text-white w-16 h-16">/</button>
-                    <button  className="bg-gray-500 rounded-full text-white w-16 h-16">=</button>
                 </div>
             </div>
         </div>
